2d909ee1b895d8974b5a90f73cc47a6ec9e69670
Mohamed Elkhair <54824413+Simohjc@users.noreply.github.com>
2025-01-08 02:19:13 -0500

    Update index.js
    add functions click to the button 
    function to truck how many clicks on every sections

---
+++ Moroccan Makla/index.js
@@ -0,0 +1,14 @@
+
+const buttons = document.querySelectorAll('.click-btn');
+
+buttons.forEach(button => {
+    const buttonId = button.getAttribute('data-id');
+    const countDisplay = document.getElementById("click-count-1");
+    let count = parseInt(localStorage.getItem(`click-count-${buttonId}`)) || 0;
+    countDisplay.textContent = `Checked out: ${count}`;
+    button.addEventListener('click', () => {
+        count++;
+        countDisplay.textContent = `Checked out: ${count}`;
+        localStorage.setItem(`click-count-${buttonId}`, count);
+    });
+});
