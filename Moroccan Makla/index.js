552ce50b86fc56d0ec44e9b2a305e06fc607c641
Mohamed Elkhair <54824413+Simohjc@users.noreply.github.com>
2025-01-08 02:37:10 -0500

    Update index.js
    just little update

--- Moroccan Makla/index.js
+++ Moroccan Makla/index.js
@@ -1,9 +1,10 @@
 
+
 const buttons = document.querySelectorAll('.click-btn');
 
 buttons.forEach(button => {
     const buttonId = button.getAttribute('data-id');
-    const countDisplay = document.getElementById("click-count-1");
+    const countDisplay = document.getElementById(`click-count-${buttonId}`);
     let count = parseInt(localStorage.getItem(`click-count-${buttonId}`)) || 0;
     countDisplay.textContent = `Checked out: ${count}`;
     button.addEventListener('click', () => {
@@ -12,3 +13,4 @@ buttons.forEach(button => {
         localStorage.setItem(`click-count-${buttonId}`, count);
     });
 });
+
